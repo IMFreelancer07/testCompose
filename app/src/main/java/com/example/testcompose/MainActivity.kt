926689e4d9403472575f7491e52d7bc83b0c0385
IMFreelancer07 <imfreelancer07@gmail.com>
2023-07-14 23:31:06 +0500

    tween animation!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,13 +3,24 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
@@ -20,11 +31,35 @@ import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
 import androidx.constraintlayout.compose.Dimension
 import com.example.testcompose.ui.theme.TestComposeTheme
+import kotlinx.coroutines.delay
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+
+            var sizeState by remember {
+                mutableStateOf(200.dp)
+            }
+            val size by animateDpAsState(targetValue = sizeState,
+
+                tween(
+                    durationMillis = 3000,
+                    delayMillis = 300,
+                    easing = LinearOutSlowInEasing
+                )
+            )
+
+            Box (modifier = Modifier
+                .size(size)
+                .background(Color.Red),
+                contentAlignment = Alignment.Center) {
+                Button(onClick = {
+                    sizeState += 50.dp
+                }) {
+                    Text(text = "Box Size ++")
+                }
+            }
 
         }
     }
