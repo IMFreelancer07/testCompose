04398b0468dc07488fcbb48a7fcaa27824c30345
IMFreelancer07 <imfreelancer07@gmail.com>
2023-07-15 00:16:31 +0500

    Circular ProgressBar!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -1,129 +1,95 @@
 package com.example.testcompose
 
 import android.os.Bundle
+import android.transition.CircularPropagation
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.FastOutLinearInEasing
-import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.LinearOutSlowInEasing
-import androidx.compose.animation.core.RepeatMode
-import androidx.compose.animation.core.Spring
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.infiniteRepeatable
-import androidx.compose.animation.core.keyframes
-import androidx.compose.animation.core.rememberInfiniteTransition
-import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.background
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.*
 import androidx.compose.runtime.setValue
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
-import androidx.constraintlayout.compose.ChainStyle
-import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.constraintlayout.compose.ConstraintSet
-import androidx.constraintlayout.compose.Dimension
-import com.example.testcompose.ui.theme.TestComposeTheme
-import kotlinx.coroutines.delay
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            var sizeState by remember {
-                mutableStateOf(200.dp)
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier.fillMaxSize()
+            ) {
+                CircularProgressBar(percentage = 0.7f, number = 360)
             }
-            val size by animateDpAsState(targetValue = sizeState,
-
-                tween(
-                    durationMillis = 1000
-                )
-            )
-
-            val infiniteTransition = rememberInfiniteTransition()
-            val color by infiniteTransition.animateColor(
-                initialValue = Color.Red,
-                targetValue = Color.Green,
-                animationSpec = infiniteRepeatable(
-                    tween(
-                        durationMillis = 2000
-                    ),
-                    repeatMode = RepeatMode.Reverse
-                )
-            )
-
-            Box (modifier = Modifier
-                .size(size)
-                .background(color),
-                contentAlignment = Alignment.Center) {
-                Button(onClick = {
-                    sizeState += 50.dp
-                }) {
-                    Text(text = "Box Size ++")
-                }
-            }
-
         }
     }
 }
 
-/** Constraint Layout **/
-/**
- * setContent {
-val constraints = ConstraintSet {
-val greenBox = createRefFor("greenbox")
-val blueBox = createRefFor("bluebox")
-val guideline = createGuidelineFromTop(0.5f)
+@Composable
+fun CircularProgressBar(
+    percentage: Float ,
+    number: Int,
+    fontSize : TextUnit = 28.sp,
+    radius : Dp = 50.dp,
+    color : Color = Color.Cyan,
+    strockWidth: Dp = 8.dp,
+    animationDuration : Int = 1000,
+    animationDelay : Int = 0
+    ){
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val currentPercentage = animateFloatAsState(
+        targetValue = if(animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animationDuration,
+            delayMillis = animationDelay
+        )
+    )
 
-constrain(greenBox) {
-top.linkTo(guideline)
-start.linkTo(parent.start)
-width = Dimension.value(100.dp)
-height = Dimension.value(100.dp)
+    LaunchedEffect(key1 = true ) {
+        animationPlayed = true
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f)
+    ) {
+        Canvas(modifier = Modifier.size(radius * 2f)) {
+            drawArc(
+                color = color,
+                -90f,
+                360 * currentPercentage.value,
+                useCenter = false,
+                style = Stroke(strockWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+
+        Text(
+            text = (currentPercentage.value * number).toInt().toString(),
+            fontSize = fontSize,
+            color = Color.Black,
+            fontWeight = FontWeight.Bold
+        )
+
+    }
 
 }
-
-constrain(blueBox) {
-top.linkTo(parent.top)
-start.linkTo(greenBox.end)
-end.linkTo(parent.end)
-width = Dimension.value(100.dp)
-height = Dimension.value(100.dp)
-
-}
-createHorizontalChain(greenBox,blueBox, chainStyle = ChainStyle.Packed)
-}
-
-ConstraintLayout(constraints,modifier = Modifier.fillMaxSize()) {
-Box(
-modifier = Modifier
-.background(Color.Green)
-.layoutId("greenbox")
-)
-
-Box(
-modifier = Modifier
-.background(Color.Blue)
-.layoutId("bluebox")
-)
-}
-
-}
- **/
