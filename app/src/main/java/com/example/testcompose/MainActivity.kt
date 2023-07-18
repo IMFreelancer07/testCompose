1332838987edf9b03ed643e60a65e54669440fa0
IMFreelancer07 <imfreelancer07@gmail.com>
2023-07-19 00:06:11 +0500

    Navigation!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -77,6 +77,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            Navigation()
         }
     }
 }
