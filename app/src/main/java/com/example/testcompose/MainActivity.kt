5e706f5b332697c82f80e192c3aeffa5a2b62dfc
IMFreelancer07 <imfreelancer07@gmail.com>
2023-07-26 00:05:45 +0500

    mutliSelect LazyColumn!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -6,13 +6,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,6 +45,49 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            var items by remember {
+                mutableStateOf(
+                    (1..20).map{
+                        ListItem(
+                            title = "Item # $it",
+                            isSelected = false
+                        )
+                    }
+                )
+            }
+             items.filter {it.isSelected}
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxSize()
+            ) {
+                items(items.size){i ->
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .clickable {
+                                items = items.mapIndexed { j, item ->
+                                    if(i==j) {
+                                        item.copy(isSelected = !item.isSelected)
+                                    } else item
+                                }
+                            }
+                            .padding(16.dp),
+                        horizontalArrangement = Arrangement.SpaceBetween,
+                        verticalAlignment = Alignment.CenterVertically
+                    ){
+                        Text(items[i].title)
+                        if(items[i].isSelected){
+                            Icon(
+                                imageVector = Icons.Default.Check,
+                                contentDescription = "Selected",
+                                tint = Color.Green,
+                                modifier = Modifier
+                                    .size(20.dp)
+                            )
+                        }
+                    }
+                }
+            }
         }
     }
 }
