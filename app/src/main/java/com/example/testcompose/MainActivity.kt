ed2559778d84e8e91f0ee0bb084396596e885a49
IMFreelancer07 <imfreelancer07@gmail.com>
2023-07-29 23:41:48 +0500

    wip!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -72,69 +72,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            DestinationsNavHost(navGraph = NavGraphs.root)
         }
-    }
-}
-@Destination(start= true)
-@Composable
-fun LoginScreen(
-    navigator : DestinationsNavigator
-) {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Login Screen")
-        Button(onClick = {
-            navigator.navigate(
-                ProfileScreenDestination(
-                    User(
-                        name = "Syed",
-                        id = "userId",
-                        created = LocalDateTime.now()
-                    )
-                )
-            )
-        }) {
-            Text("Go to Profile Screen")
-        }
-    }
-}
-
-@Destination
-@Composable
-fun ProfileScreen(
-    navigator : DestinationsNavigator,
-    user : User
-) {
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Profile Screen: $user", textAlign = TextAlign.Center)
-        Button(onClick = {
-            navigator.navigate(
-                PostScreenDestination()
-            )
-        }) {
-            Text("Go to Post Screen")
-        }
-    }
-}
-
-@Destination
-@Composable
-fun PostScreen(
-    showOnlyPostsByUser: Boolean = false
-) {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Text(text = "Post Screen, $showOnlyPostsByUser")
     }
 }
