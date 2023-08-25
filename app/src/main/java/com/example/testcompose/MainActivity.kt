50aafc36f0053791a72f369dffcc0181eb944a1d
IMFreelancer07 <imfreelancer07@gmail.com>
2023-08-25 19:43:28 +0500

    Material3UX-NavigationDrawers!

--- app/src/main/java/com/example/testcompose/MainActivity.kt
+++ app/src/main/java/com/example/testcompose/MainActivity.kt
@@ -3,25 +3,161 @@ package com.example.testcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Message
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Message
+import androidx.compose.material.icons.outlined.Settings
+import androidx.compose.material3.DismissibleNavigationDrawer
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.NavigationDrawerItemDefaults
+import androidx.compose.material3.PermanentNavigationDrawer
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
+
+data class NavigationItem(
+    val title: String,
+    val unSelectedIcon: ImageVector,
+    val selectedIcon: ImageVector,
+    val hasNews: Boolean,
+    val badgeCount: Int? = null
+)
 
 class MainActivity : ComponentActivity() {
 
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
 
+            val items = listOf(
+                NavigationItem(
+                    title = "Home",
+                    selectedIcon = Icons.Filled.Home,
+                    unSelectedIcon = Icons.Outlined.Home,
+                    hasNews = false,
+                ),
+                NavigationItem(
+                    title = "Chats",
+                    selectedIcon = Icons.Filled.Message,
+                    unSelectedIcon = Icons.Outlined.Message,
+                    hasNews = false,
+                    badgeCount = 50
+                ),
+                NavigationItem(
+                    title = "Settings",
+                    selectedIcon = Icons.Filled.Settings,
+                    unSelectedIcon = Icons.Outlined.Settings,
+                    hasNews = true,
+                )
+            )
+
             Surface(
                 modifier = Modifier.fillMaxSize(),
                 color = MaterialTheme.colorScheme.background
             ) {
-                Text("Work in progress")
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
+                val scope = rememberCoroutineScope()
+                var selectedItemIndex by rememberSaveable {
+                    mutableStateOf(0)
+                }
+                /**
+                PermanentNavigationDrawer // used for showing permanently; just have to replace with ModalNavigationDrawer
+                                            and have to comment the Navigationstate as there is no need to open/close it.
+                **/
+                /**
+                DismissibleNavigationDrawer // a mixup of Modal and Permanent Drawer; But can be dismissed,
+                                                as we can't in case of Permanent one.
+                DismissibleDrawerSheet // This one will replace ModalDrawerSheet.
+                 **/
+
+                ModalNavigationDrawer(
+                    drawerContent = {
+                        ModalDrawerSheet {
+                            Spacer(modifier = Modifier.height(16.dp))
+                            items.forEachIndexed { index, navigationItem ->
+                                NavigationDrawerItem(
+                                    label = { Text(text = navigationItem.title) },
+                                    selected = index == selectedItemIndex,
+                                    onClick = {
+                                        selectedItemIndex = index
+                                        scope.launch {
+                                            drawerState.close()
+                                        }
+                                    },
+                                    icon = {
+                                        Icon(
+                                            imageVector = if (index == selectedItemIndex) {
+                                                navigationItem.selectedIcon
+                                            } else navigationItem.unSelectedIcon,
+                                            contentDescription = navigationItem.title
+                                        )
+                                    },
+                                    badge = {
+                                        navigationItem.badgeCount?.let {
+                                            Text(text = navigationItem.badgeCount.toString())
+                                        }
+                                    },
+                                    modifier = Modifier
+                                        .padding(NavigationDrawerItemDefaults.ItemPadding)
+                                )
+                            }
+                        }
+                    },
+                    drawerState = drawerState,
+                ) {
+                    Scaffold(
+                        topBar = {
+                            TopAppBar(
+                                title = {
+                                    Text(text = "NavDrawer")
+                                },
+                                navigationIcon = {
+                                    IconButton(onClick = {
+                                        scope.launch {
+                                            drawerState.open()
+                                        }
+                                    }) {
+                                        Icon(
+                                            imageVector = Icons.Default.Menu,
+                                            contentDescription = "Menu"
+                                        )
+                                    }
+                                }
+                            )
+                        }
+                    ) {
+
+                    }
+                }
             }
         }
     }
